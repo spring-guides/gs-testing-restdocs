858388b982cb14eb8eb1643578c77b73e611674f
Jay Bryant <jbryant@pivotal.io>
2019-10-25 15:16:27 -0500

    Upgraded to Spring Boot 2.2
    I upgraded the build files to Spring Boot 2.2.0, which necessitated changing the tests to use JUnit 5.

--- complete/src/test/java/com/example/testingrestdocs/WebLayerTest.java
+++ complete/src/test/java/com/example/testingrestdocs/WebLayerTest.java
@@ -1,12 +1,10 @@
 package com.example.testingrestdocs;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
@@ -16,7 +14,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
 @WebMvcTest(HomeController.class)
 @AutoConfigureRestDocs(outputDir = "target/snippets")
 public class WebLayerTest {
