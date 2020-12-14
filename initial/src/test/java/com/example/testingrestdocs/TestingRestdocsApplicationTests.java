1cf291b3bafd04dfa4e51cc0281fe488f6fcdf84
Bradley Turek <UgmaDevelopment@gmail.com>
2020-12-14 10:09:47 -0700

    Uses Initializr Rest Docs maven configuration.
    * Adds a .gitignore
    * Adds configuration to `pom` for asciidoctor and restdocs
    * Updates the maven wrapper from 0.5.2 to 0.5.6

--- initial/src/test/java/com/example/testingrestdocs/TestingRestdocsApplicationTests.java
+++ initial/src/test/java/com/example/testingrestdocs/TestingRestdocsApplicationTests.java
@@ -1,14 +1,13 @@
 package com.example.testingrestdocs;
 
 import org.junit.jupiter.api.Test;
-
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class TestingRestdocsApplicationTests {
+class TestingRestdocsApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	void contextLoads() {
 	}
 
 }
